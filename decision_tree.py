39d9ba82c23f5c557b8df46d000fce54a67a2275
Ohranj <ajdorrington@hotmail.co.uk>
2020-11-13 19:54:48 +0000

    initial commit

--- decision_tree.py
+++ decision_tree.py
@@ -9,45 +9,46 @@ from sklearn.model_selection import train_test_split
 url = 'https://earthquake.usgs.gov/fdsnws/event/1/query'
 params = {'format': 'geojson', 'starttime': '2019-06-01', 'endtime': '2019-12-31', 'minlatitude': '18', 'maxlatitude': '54', 'minlongitude': '73', 'maxlongitude': '135'}
 
-fetchData = np.array(requests.get(url, params=params).json()['features'])
+fetchData = np.array(requests.get(url, params).json()['features'])
 
 data = []
 for i in fetchData:
-    data.append({'mag': i['properties']['mag']})
+    data.append({'mag': i['properties']['mag'], 'time': i['properties']['time']})
 
 data.reverse()
 data = pd.DataFrame(data)
 
 df = data[['mag']]
 
-
-#Create variable to predict into future
-future_days = 50
+#Create variable to predict into future - From the start of December gathered using unix time
+days_to_predict = len(data[data['time'] > 1575158400000])
 #Create a new column with target data shifted x units
-df['Prediction'] = df[['mag']].shift(-future_days)
+df['Prediction'] = df[['mag']].shift(-days_to_predict)
 #Create feature dataset (X) and convert to np array and remove last x rows
-X = np.array(df.drop(['Prediction'], 1))[:-future_days]
+X = np.array(df.drop(['Prediction'], 1))[:-days_to_predict]
 #Create target dataset (y) and convert to np array get target values except last x rows
-y = np.array(df['Prediction'])[:-future_days]
+y = np.array(df['Prediction'])[:-days_to_predict]
 #Split the data into 75% training and 25% testing
-x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.23)
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
 #Create decision tree regressor model
-tree = DecisionTreeRegressor().fit(x_train, y_train)
+Decision_tree = DecisionTreeRegressor().fit(x_train, y_train)
 #Get the last x rows of the feature dataset
-x_future = df.drop(['Prediction'], 1)[:-future_days]
-x_future = x_future.tail(future_days)
+x_future = df.drop(['Prediction'], 1)[:-days_to_predict]
+x_future = x_future.tail(days_to_predict)
 x_future = np.array(x_future)
 #Show the model tree prediction
-tree_prediction = tree.predict(x_future)
+tree_prediction = Decision_tree.predict(x_future)
 #Visualise the data
 predictions = tree_prediction
 valid = df[X.shape[0]:]
 valid['Predictions'] = predictions
 
-plt.figure(figsize=(14, 7))
-plt.xlabel('Time')
+plt.figure(figsize=(12, 6))
+plt.title('Decision tree prediction of earthquake magnitudes occuring throughout December 2019 - China')
+plt.xlabel('Days')
 plt.ylabel('Magnitude')
 plt.plot(df['mag'])
 plt.plot(valid[['mag', 'Predictions']])
-plt.legend(['Orig', 'val', 'pred'])
+plt.legend(['Testing data', 'Training data', 'prediction'])
+plt.xlim((0,436))
 plt.show()
--- lr_regress.py
+++ lr_regress.py
@@ -28,7 +28,7 @@ training_data_len = math.ceil(len(dataset) *.8)
 scaler = MinMaxScaler(feature_range=(0, 1)) 
 scaled_data = scaler.fit_transform(dataset)
 
-train_data = scaled_data[0:training_data_len  , : ]
+train_data = scaled_data[0:training_data_len, :]
 
 x_train=[]
 y_train = []
@@ -39,23 +39,22 @@ for i in range(30,len(train_data)):
 
 x_train, y_train = np.array(x_train), np.array(y_train)
 
-
 x_train = np.reshape(x_train, (x_train.shape[0],x_train.shape[1],1))
 
-model = Sequential()
-model.add(LSTM(units=50, return_sequences=True,input_shape=(x_train.shape[1],1)))
-model.add(LSTM(units=50, return_sequences=False))
-model.add(Dense(units=25))
-model.add(Dense(units=1))
+LSTM_model = Sequential()
+LSTM_model.add(LSTM(units=50, return_sequences=True,input_shape=(x_train.shape[1],1)))
+LSTM_model.add(LSTM(units=50, return_sequences=False))
+LSTM_model.add(Dense(units=25))
+LSTM_model.add(Dense(units=1))
 
-model.compile(optimizer='adam', loss='mean_squared_error')
+LSTM_model.compile(optimizer='adam', loss='mean_squared_error')
 
-model.fit(x_train, y_train, batch_size=1, epochs=1)
+LSTM_model.fit(x_train, y_train, batch_size=32, epochs=5)
 
-test_data = scaled_data[training_data_len - 30: , : ]
+test_data = scaled_data[training_data_len - 30:,: ]
 
 x_test = []
-y_test =  dataset[training_data_len : , : ] #Get all of the rows from index 1603 to the rest and all of the columns (in this case it's only column 'Close'), so 2003 - 1603 = 400 rows of data
+y_test =  dataset[training_data_len:, :] 
 for i in range(30,len(test_data)):
     x_test.append(test_data[i-30:i,0])
 
@@ -63,30 +62,26 @@ x_test = np.array(x_test)
 
 x_test = np.reshape(x_test, (x_test.shape[0],x_test.shape[1],1))
 
-predictions = model.predict(x_test) 
+predictions = LSTM_model.predict(x_test) 
 predictions = scaler.inverse_transform(predictions)
 
 # rmse=np.sqrt(np.mean(((predictions- y_test)**2)))
 
 # print(rmse)
 
+# rollingMean = df[['Max magnitude']].rolling(6).mean()
+
+
 train = data[:training_data_len]
 valid = data[training_data_len:]
 valid['Predictions'] = predictions
 
 plt.figure(figsize=(16,8))
-plt.title('Model')
+plt.title('LSTM prediction for proceeding years highest magnitude')
 plt.xlabel('Date', fontsize=18)
 plt.ylabel('Max magnitude', fontsize=18)
 plt.plot(train['Max magnitude'])
 plt.plot(valid[['Max magnitude', 'Predictions']])
-plt.legend(['Train', 'Actual', 'Predictions'], loc='lower right')
+plt.grid(True, axis='y')
+plt.legend(['Rolling mean', 'Training data', 'Actual data', 'Predictions'], loc='lower right')
 plt.show()
-
-
-
-
-
-#rollingMean = df[['Max magnitude']].rolling(6).mean()
-#plt.plot(rollingMean)
-#plt.show()
